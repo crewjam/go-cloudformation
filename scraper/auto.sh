34707e3ade612c8d0765c9120cbbcdc2a14a1940
Ross Kinder <ross@kndr.org>
2015-12-18 10:08:03 -0500

    add script to regenerate and commit upstream schema changes automatically

---
+++ scraper/auto.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# make sure we have a clean checkout and the latest changes
+git diff --exit-code || (echo "cannot run $0 with pending changes"; exit 1)
+git fetch origin master -q
+git checkout FETCH_HEAD -q
+
+# re-run the code generator
+[ ! -d .scraper-cache ] || rm -rf .scraper-cache
+go generate .
+
+# if nothing changed, we're done
+if git diff --exit-code >/dev/null ; then
+	exit 0
+fi
+
+# abort if we broke something
+go test ./...
+
+# commit and apply the changes
+git add -A
+git commit -m "schema: apply changes from AWS documentation updates"
+git push
