cf43b843630e0b85f3b82f3e1da963907a304b02
Ross Kinder <ross@kndr.org>
2016-05-06 08:20:55 -0400

    remove dependency on github.com/kr/pretty which is broken on go1.5

--- roundtrip_test.go
+++ roundtrip_test.go
@@ -3,9 +3,8 @@ package cloudformation
 import (
 	"encoding/json"
 	"io/ioutil"
+	"reflect"
 	"testing"
-
-	"github.com/kr/pretty"
 )
 
 var testFilePaths = []string{
@@ -70,8 +69,7 @@ func testOne(t *testing.T, input []byte) {
 	parsedOutput := map[string]interface{}{}
 	json.Unmarshal(output, &parsedOutput)
 
-	diffs := pretty.Diff(parsedInput, parsedOutput)
-	for _, diff := range diffs {
-		t.Errorf("%s", diff)
+	if !reflect.DeepEqual(parsedInput, parsedOutput) {
+		t.Errorf("expected %#v, got %#v", parsedInput, parsedOutput)
 	}
 }
