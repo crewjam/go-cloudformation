04438440689157e0718fa66fd5d341d1ee547f38
Ross Kinder <ross@kndr.org>
2016-07-06 13:29:08 -0400

    update IAM data types

--- iam_policy.go
+++ iam_policy.go
@@ -9,14 +9,15 @@ type IAMPolicyDocument struct {
 // IAMPrincipal represents a principal in an IAM policy
 type IAMPrincipal struct {
 	Service *StringListExpr `json:",omitempty"`
+	AWS     *StringListExpr `json:",omitempty"`
 }
 
 // IAMPolicyStatement represents an IAM policy statement
 type IAMPolicyStatement struct {
-	Sid            string          `json:",omitempty"`
-	Effect         string          `json:",omitempty"`
-	Principal      *IAMPrincipal   `json:",omitempty"`
-	Action         *StringListExpr `json:",omitempty"`
-	Resource       *StringListExpr `json:",omitempty"`
-	ConditionBlock interface{}     `json:",omitempty"`
+	Sid       string          `json:",omitempty"`
+	Effect    string          `json:",omitempty"`
+	Principal *IAMPrincipal   `json:",omitempty"`
+	Action    *StringListExpr `json:",omitempty"`
+	Resource  *StringListExpr `json:",omitempty"`
+	Condition interface{}     `json:",omitempty"`
 }
